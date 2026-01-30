324d24901a13de4244361f9fbcfdb3cf5f7b8e46
jungsehui202 <jung.sehui@linkareer.com>
2026-01-30 11:52:38 +0900

    feat: graphql 모듈 임포트

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    // GraphQL 모듈 추가
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true, // GraphQL Playground 활성화
+      introspection: true,
+    }),
+    PrismaModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
