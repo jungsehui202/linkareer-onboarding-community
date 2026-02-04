04cdd08ed6303220268c998f07d5f49a74d27ad0
jungsehui202 <jung.sehui@linkareer.com>
2026-02-04 16:59:14 +0900

    refactor(user): DTO 파일 수정 및 유저 서비스 로직 수정

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,8 +6,27 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log:
+        process.env.LOG_PRISMA_QUERY === 'true'
+          ? ['query', 'info', 'warn', 'error'] // 개발: 모든 쿼리 확인
+          : ['error'], // 운영: 에러만
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+
+    if (process.env.NODE_ENV !== 'production') {
+      this.$on('query' as never, (e: any) => {
+        console.log('='.repeat(60));
+        console.log('Query:', e.query);
+        console.log('Params:', e.params);
+        console.log('Duration:', e.duration + 'ms');
+        console.log('='.repeat(60));
+      });
+    }
   }
 
   async onModuleDestroy() {
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserService } from './application/user.service';
-import { UserPrisma } from './domain/user.prisma';
 import { UserResolver } from './presentation/user.resolver';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UserPrisma, UserService, UserResolver],
-  exports: [UserService, UserPrisma],
+  providers: [UserService, UserResolver],
+  exports: [UserService],
 })
 export class UserModule {}
